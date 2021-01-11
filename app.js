5a0e0cd6e25bf22c9a0dd3cfd32603fdf55f6725
Rishabh Mishra <rismis0505@gmail.com>
2021-01-11 23:18:07 +0530

    User Auth with passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 
 var indexRouter = require('./routes/index');
@@ -46,30 +48,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 function auth (req, res, next) {
-      console.log(req.session);
-  
-    if(!req.session.user) {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
-    }
-    else {
-      if (req.session.user === 'authenticated') {
-        next();
-      }
-      else {
-        var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
-      }
-    }
+  console.log(req.user);
+
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
-  
-
-    
-
+  else {
+        next();
+  }
+}
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
